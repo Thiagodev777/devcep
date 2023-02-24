b656ceed3018c58e5dbadfd6cbf4e45c2d006027
Antonio Thiago <antoniobackenddeveloper@gmail.com>
2023-02-24 20:57:27 -0300

    Feat: header implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import Header from "./Components/Header/Header";
+import Clock from "./Components/Clock/Clock";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <React.Fragment>
+      <Header />
+      <Clock />
+    </React.Fragment>
+  );
 };
 
 export default App;
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./style.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>DEVCEP</h1>
+    </header>
+  );
+};
+
+export default Header;
